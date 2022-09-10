85aac8061d28a15ab9e9886cfb8499f7ddf7d7c8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-11 00:12:56 +0200

    liblineage: hudson: build_target: Add get_next_build_date
    Change-Id: Ie2fdae8ce4ce14c3e13022013b09793276dc3ea9

--- liblineage/hudson/build_target.py
+++ liblineage/hudson/build_target.py
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 #
 
+from datetime import date, timedelta
+from random import Random
 import requests
 
 from liblineage.constants.infra import GITHUB_ORG
@@ -41,3 +43,34 @@ class BuildTarget:
 		"""Get the build target given a device codename."""
 		# There can't be duplicates, plus this will miserably fail if this device isn't there
 		return [target for target in cls.get_lineage_build_targets() if target.device == device][0]
+
+	def get_next_build_date(self) -> date:
+		"""Get the next build date for this build target."""
+		today = date.today()
+
+		if self.period == Period.NIGHTLY:
+			return today + timedelta(days=1)
+
+		random = Random()
+		random.seed(self.device, version=1)
+
+		if self.period == Period.WEEKLY:
+			day_of_week = int(1+7*random.random())
+			delta_day = day_of_week - today.isoweekday()
+
+			if delta_day <= 0:
+				# Go to next week
+				delta_day += 7
+
+			return today + timedelta(days=delta_day)
+
+		if self.period == Period.MONTHLY:
+			day_of_month = int(1+28*random.random())
+			delta_day = day_of_month - today.day
+
+			if delta_day <= 0:
+				# Go to next month
+				month_to_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+				delta_day += month_to_days[today.month-1]
+
+			return today + timedelta(days=delta_day)
