14155b3b00c60389183ae4537c99759992c97fa7
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-06 16:37:10 +0100

    liblineage: ota: Use the new updater API and deprecate this class
    Change-Id: I5588732b7910f2d45b223f3b3b6a8e40a56ae8a8

--- liblineage/ota/full_update_info.py
+++ liblineage/ota/full_update_info.py
@@ -5,14 +5,11 @@
 #
 
 from datetime import datetime
-import requests
 from typing import Dict
 
-from liblineage.constants.infra import DOMAIN
 from liblineage.constants.versions import LINEAGEOS_TO_ANDROID_VERSION
-
-API_VERSION = "v1"
-API_URL = f"https://download.{DOMAIN}/api/{API_VERSION}"
+from liblineage.updater.v1 import SyncV1Api
+from liblineage.updater.v1.build import Build
 
 class FullUpdateInfo:
 	"""LineageOS full update information.
@@ -25,16 +22,20 @@ class FullUpdateInfo:
 	- size (int): The size of the update (bytes)
 	- url (str): The URL of the update
 	- version (str): The LineageOS version of the update (e.g. 18.1)
+
+	Warning: This class is deprecated and will be removed in a future release.
+	Please move to liblineage.updater.
 	"""
-	def __init__(self,
-		         datetime: datetime,
-		         filename: str,
-				 id: str,
-				 romtype: str,
-				 size: int,
-				 url: str,
-				 version: str,
-		        ):
+	def __init__(
+		self,
+		datetime: datetime,
+		filename: str,
+		id: str,
+		romtype: str,
+		size: int,
+		url: str,
+		version: str,
+	):
 		"""Initialize the full update information."""
 		self.datetime = datetime
 		self.filename = filename
@@ -47,17 +48,44 @@ class FullUpdateInfo:
 		self.android_version = LINEAGEOS_TO_ANDROID_VERSION[version]
 
 	@classmethod
-	def from_json(self, update: Dict):
-		"""Create a full update information object from a JSON object."""
-		return self(datetime.fromtimestamp(update["datetime"]), update["filename"],
-		            update["id"], update["romtype"],
-		            update["size"], update["url"], update["version"])
+	def from_json(cls, update: Dict):
+		"""Create a full update information object from a JSON object.
+
+		Warning: This class is deprecated and will be removed in a future release.
+		Please move to liblineage.updater.
+		"""
+		return cls(
+			datetime.fromtimestamp(update["datetime"]),
+			update["filename"],
+			update["id"],
+			update["romtype"],
+			update["size"],
+			update["url"],
+			update["version"]
+		)
+
+	@classmethod
+	def from_V1_build(cls, build: Build):
+		"""Create a full update information object from a V1 build object.
+
+		Warning: This class is deprecated and will be removed in a future release.
+		Please move to liblineage.updater."""
+		return cls(
+			build.datetime,
+			build.filename,
+			build.id,
+			build.romtype,
+			build.size,
+			build.url,
+			build.version
+		)
 
 	@classmethod
 	def get_nightlies(cls, device: str):
-		"""Get the latest OTAs for a device."""
-		url = f"{API_URL}/{device}/nightly/1"
-		response = requests.get(url=url).json()["response"]
-		updates = [cls.from_json(update) for update in response]
+		"""Get the latest OTAs for a device.
 
-		return updates
+		Warning: This class is deprecated and will be removed in a future release.
+		Please move to liblineage.updater."""
+		builds = SyncV1Api.get_device_builds(device, "nightly", "1")
+
+		return [cls.from_V1_build(build) for build in builds]
