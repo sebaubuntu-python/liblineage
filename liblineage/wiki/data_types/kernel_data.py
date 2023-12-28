82dab558f4070daefc65993c859557efa91e503b
Giovanni Ricca <giovanniricca@proton.me>
2023-12-28 18:36:15 +0100

    liblineage: Unbreak it (again)
    * Following the changes on https://github.com/LineageOS/lineage_wiki/commit/858d862ad4127a5c2928c87a287748657adbd7b0
    
    Change-Id: Id1cd2f06c600f0afca8b0e1f898c051a5fecfdc1

---
+++ liblineage/wiki/data_types/kernel_data.py
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+#
+
+from typing import Dict, Union
+
+from liblineage.constants.infra import GITHUB_ORG_URL
+from liblineage.wiki.data_types.base_data import BaseData
+
+class KernelData(BaseData):
+	"""The format required for the kernel property.
+
+	Attributes:
+	- repo: Kernel repo
+	- version: Kernel version
+	"""
+	def __init__(self,
+	             repo: str,
+	             version: float,
+	            ):
+		"""Initialize the kernel information."""
+		super().__init__()
+
+		self.repo = f"{GITHUB_ORG_URL}/{repo}"
+		self.version = version
+
+	@classmethod
+	def from_data(cls, data: Union[None, Dict]) -> Union[None, "KernelData"]:
+		"""Create a kernel information object from YAML data."""
+		if data is None:
+			kernel = None
+		elif isinstance(data, dict):
+			kernel = cls.from_dict(data)
+		else:
+			raise Exception("Invalid kernel data")
+
+		return kernel
