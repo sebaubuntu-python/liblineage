49e93c56a68454b38b2c9539dadf8a8cf6a414ce
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-12-31 01:21:05 +0100

    liblineage: Update dependencies
    Change-Id: Iafa505c3851e7bbbe63d6568d726e0fffa01a4df

--- liblineage/constants/versions.py
+++ liblineage/constants/versions.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 #
@@ -23,6 +23,8 @@ for key, value in {
 	"20.0": AndroidVersion.TIRAMISU,
 	"21": AndroidVersion.UPSIDE_DOWN_CAKE,
 	"21.0": AndroidVersion.UPSIDE_DOWN_CAKE,
+	"22.0": AndroidVersion.VANILLA_ICE_CREAM,
+	"22.1": AndroidVersion.VANILLA_ICE_CREAM,
 }.items():
 	LINEAGEOS_TO_ANDROID_VERSION[key] = value
 	ANDROID_TO_LINEAGEOS_VERSION[value] = key
